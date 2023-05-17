71307a43850273f0ac3a0bef416f36e4004ea122
shuajbkingu <shuajbmemaa@hotmail.com>
2023-05-18 00:03:08 +0200

    logini

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -42,29 +42,53 @@ const Login = () => {
         .then(err => console.log(err))
     }
 
-  return (
-    <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
-        <div className='bg-white p-3 rounded w-25'>
-            <h2>Log In</h2>
+    return (
+        <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
+          <div className="bg-white p-3 rounded w-25">
+            <h2 className="text-center mb-4">Log In</h2>
             <form onSubmit={handleSubmit}>
-            <div className='mb-3'>
-                <label htmlFor="email"><strong>Email</strong></label>
-                <input type="email" placeholder='Shtyp Emailin tuaj' name='email'
-                onChange={handleInput} className='form-control rounded-0'/>
-            </div>
-            <div className='mb-3'>
-                <label htmlFor="password"><strong>Password</strong></label>
-                <input type="password" placeholder='Shtyp passwordin tuaj' name='password'
-                onChange={handleInput} className='form-control rounded-0' />
-            </div>
-            <button type='submit' className='btn btn-success w-100 rounded-0'>Sign Up</button>
-            <p>Pranoni termet tona</p>
-            <Link to="/register" className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Create Account</Link>
-
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  placeholder="Shtyp Emailin tuaj"
+                  name="email"
+                  value={values.email}
+                  onChange={handleInput}
+                  className="form-control rounded-0"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  placeholder="Shtyp passwordin tuaj"
+                  name="password"
+                  value={values.password}
+                  onChange={handleInput}
+                  className="form-control rounded-0"
+                />
+              </div>
+              <button type="submit" className="btn btn-success w-100 rounded-0">
+                Log In
+              </button>
+              <p className="text-center my-2">
+                Pranoni termet tona
+              </p>
+              <Link
+                to="/register"
+                className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none"
+              >
+                Create Account
+              </Link>
             </form>
+          </div>
         </div>
-    </div>
-  )
-}
+      );
+    };
 
 export default Login
