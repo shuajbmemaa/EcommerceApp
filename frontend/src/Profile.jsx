27eb4b5ba8be20827c8529cccbd3b6123f4e9862
shuajbkingu <shuajbmemaa@hotmail.com>
2023-05-22 20:44:56 +0200

    Profili

--- frontend/src/Profile.jsx
+++ frontend/src/Profile.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Profile = () => {
+  const [userData, setUserData] = useState({});
+
+  const handleUpdate = ()=>{
+    
+  }
   return (
-    <div>Profili</div>
+    <div>
+    <h1>Profili im</h1>
+    <p>ID: {userData.id}</p>
+    <p>Email: {userData.email}</p>
+    <p>Password: {userData.password}</p>
+    <button onClick={handleUpdate}>Përditëso të dhënat</button>
+  </div>
   )
 }
 
