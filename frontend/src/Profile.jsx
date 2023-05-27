c52d31e7cee463076846a5baeda7b6eca09118d8
shuajbkingu <shuajbmemaa@hotmail.com>
2023-05-27 11:38:18 +0200

    profili

--- frontend/src/Profile.jsx
+++ frontend/src/Profile.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect } from 'react'
 import { useState } from 'react';
 
 const Profile = () => {
-  const [userData, setUserData] = useState({});
+  const [userData, setUserData] = useState([]);
 
-  const handleUpdate=(event)=>{
-    
-}
+  
   return (
-    <div>
-    <h1>Profili im</h1>
-    <p>ID: {userData.id}</p>
-    <p>Email: {userData.email}</p>
-    <p>Password: {userData.password}</p>
-    <button onClick={handleUpdate}>Perditeso te dhenat</button>
-  </div>
-  )
+    <div className='mt-4 px-5 pt-3'>
+        <h3 className='text-center'>Profili im </h3>
+        <table className="table table-bordered table-striped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userData.map(users => (
+            <tr key={users.id}>
+              <td>{users.id}</td>
+              <td>{users.name}</td>
+              <td>{users.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </div>
+  );
 }
 
 export default Profile
