d0869d6b44b43edd454edacb0aa264d604438feb
shuajbkingu <shuajbmemaa@hotmail.com>
2023-05-31 00:21:09 +0200

    kompania

--- frontend/src/Kompania.jsx
+++ frontend/src/Kompania.jsx
@@ -1,8 +1,26 @@
+import { LogoutOutlined } from '@ant-design/icons';
+import axios from 'axios';
 import React from 'react'
 
 const Kompania = () => {
+
+  const handleLogout = () => {
+    axios.get('http://localhost:8081/logoutKompani')
+      .then(res => {
+        window.location.reload();
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
-    <div>Kompania</div>
+    <div>
+      <h2>Kompania</h2>
+      <li onClick={handleLogout} style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }}>
+            <LogoutOutlined style={{ fontSize: '20px', marginRight: '5px', color: 'black' }} />
+            <span style={{ fontSize: '16px', color: 'white' }}>Logout</span>
+          </li>
+    </div>
+    
   )
 }
 
