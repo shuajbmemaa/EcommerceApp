4d48035803d4c09dd04743e17d7b98c37c36e329
Erzaaaaa <erzaa.17@hotmail.com>
2023-05-31 13:36:18 +0200

    view produktet

--- frontend/src/ProduktetView.jsx
+++ frontend/src/ProduktetView.jsx
@@ -19,9 +19,30 @@ const ProduktetView = () => {
           })
           .catch(err => console.log(err));
       },[]);
-      return (
-        <div><h2>View</h2></div>
-      )
-                }
+        return (
+          <div>
+        <tbody>
+                {produktet.map((produkti, index) => (
+                  <tr key={index}>
+                    <td>Id :{produkti.id}</td>
+                    <td>Emri :{produkti.name}</td>
+                    <td>Pershkrimi :{produkti.description}</td>
+                    <td>Cmimi :{produkti.price}</td>
+                    <td>Foto :{
+                    <img src={`http://localhost:8081/images/` + produkti.image_url} alt="" 
+                    className='produktet_image'/>
+                    }</td>
+                    <td>Stock :{produkti.stock}</td>
+                    <td>Kategori :{produkti.category_id}</td>
+                    <td>Krijuar me :{produkti.created_at}</td>
+                    <Link to="/kategorite" className='btn btn-primary'>
+                        Kthehu te Produktet
+                    </Link>
+                  </tr>
+                ))}
+              </tbody>
+              </div>
+        )
+                  }
 
 export default ProduktetView;
