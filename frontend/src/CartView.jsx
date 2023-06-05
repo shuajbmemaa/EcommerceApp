7031913a92269db2c8d1ae7a2a491fef34ee9ac8
shuajbkingu <shuajbmemaa@hotmail.com>
2023-06-05 10:07:30 +0200

    cart view

--- frontend/src/CartView.jsx
+++ frontend/src/CartView.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom';
+import './CartView.css'
 
 const CartView = () => {
     const [view, setView] = useState([]);
@@ -21,28 +22,28 @@ const CartView = () => {
 
 
   return (
-    <div>
-        <tbody>
-                {view.map((karta, index) => (
-                  <tr key={index}>
-                    <td>Id :{karta.id}</td>
-                    <td>Emri :{karta.name}</td>
-                    <td>Pershkrimi :{karta.description}</td>
-                    <td>Cmimi :{karta.price}</td>
-                    <td>Foto :{
-                    <img src={`http://localhost:8081/images/` + karta.image_url} alt="" 
-                    className='produktet_image'/>
-                    }</td>
-                    <td>Stock :{karta.stock}</td>
-                    <td>Kategori :{karta.category_id}</td>
-                    <td>Krijuar me :{karta.created_at}</td>
-                    <Link to="/" className='btn btn-primary'>
-                        Kthehu
-                    </Link>
-                  </tr>
-                ))}
-              </tbody>
-              </div>
+    <div className="cart-view">
+      {view.map((karta, index) => (
+        <div className="card" key={index}>
+          <img
+            src={`http://localhost:8081/images/` + karta.image_url}
+            alt=""
+            className="card-image"
+          />
+          <div className="card-details">
+            <h3 className="card-title">{karta.name}</h3>
+            <p className="card-description">{karta.description}</p>
+            <p className="card-price">Cmimi: {karta.price}</p>
+            <p className="card-stock">Stock: {karta.stock}</p>
+            <p className="card-category">Kategori: {karta.category_id}</p>
+            <p className="card-created">Krijuar me: {karta.created_at}</p>
+          </div>
+          <Link to="/" className="btn btn-primary">
+            Kthehu
+          </Link>
+        </div>
+      ))}
+    </div>
   )
 }
 
